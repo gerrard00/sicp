760ae307ae1cd28ff95bf9c7c75aae784983d883
Gerrard Lindsay <gerrardalindsay@gmail.com>
2017-08-25 13:29:46 -0400

    Back on exercise 1.11

--- exercise1.11.scm
+++ exercise1.11.scm
@@ -3,22 +3,38 @@
 
 ; recursive
 (define (f n)
-  ; (display n) (display "-")
+  ; (display n) (newline)
   ; (display (f (- n 3))) (display ">")
   (cond ((< n 3) n)
-         ( else (+
-             (f (- n 1))
-             (* 2 (f (- n 2)))
-             (* 3 (f (- n 3)))
-             ))
-         ))
+        (else (+
+                (f(- n 1))
+                (* 2 (f(- n 2)))
+                (* 3 (f(- n 3)))
+                ))))
 
+
+; substituting 3
 (+
-  (f (- n 1))
-  (* 2 (f (- n 2)))
-  (* 3 (f (- n 3)))
+  (f (- 3 1))
+  (* 2 (f (- 3 2)))
+  (* 3 (f (- 3 3)))
   )
 
+(+
+  (f 2)
+  (* 2 (f 1))
+  (* 3 (f 0))
+  )
+
+(+
+  2
+  (* 2 1)
+  (* 3 0)
+  )
+
+(+ 2 2 0)
+
+; substituting 4
 (+
   (f (- 4 1))
   (* 2 (f (- 4 2)))
@@ -36,22 +52,44 @@
   4
   3
   )
-
-(+ 4 4 3)
-
-; iterative
-(define (f-iter n)
-  )
-
-(define (f2 n) (f-iter (- n 1 )))
-
-(f 2)
 (f 3)
-(f 4)
-(f 5)
 (f 6)
 (f 8)
 (f 9)
 (f 10)
 (f 11)
 (f 12)
+
+; iterative
+(define(f-iter n iter count)
+  (display n)(display "-")
+  (display iter)(display "-")
+  (display count)(newline)
+  (+ (cond ((< n 3) n) (else (f2 n)))
+     (cond
+       ((= count iter)
+        (cond ((> iter 3) 0)
+              (else (f-iter (- n 1) (+ iter 1) 1)))
+        )
+       (else (f-iter n iter (+ count 1)))))
+
+  )
+
+(define (f2 n) (f-iter (- n 1) 1 1))
+
+(f 3)
+(f2 3)
+(f 4)
+(f2 4)
+(f 5)
+(f2 5)
+
+
+(define (simple n)
+  (+
+    1
+    (cond ((= n 1) 0)
+          (else (simple (- n 1))))))
+
+
+(simple 9)
